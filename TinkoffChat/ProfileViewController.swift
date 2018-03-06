ffced2d54ea5d187e8f08216dbbdf5bc5e56b2f1
Vladislav Kolesnikov <vladislav.kolesnikov@onetwotrip.com>
2018-03-06 21:03:16 +0300

    рефакторинг

--- TinkoffChat/ProfileViewController.swift
+++ TinkoffChat/ProfileViewController.swift
@@ -9,22 +9,20 @@
 import UIKit
 
 class ProfileViewController: UIViewController, UINavigationControllerDelegate, UIImagePickerControllerDelegate {
-
+    
     @IBOutlet var uiEditButton: UIButton!
     @IBOutlet var uiTitle: UILabel!
     @IBOutlet var uiProfilePicture: UIImageView!
     @IBOutlet var uiChangePhotoButton: RoundButton!
     
-    var uiAlert: UIAlertController!
     var uiImagePicker: UIImagePickerController!
     
     override func viewDidLoad() {
         super.viewDidLoad()
-    
+        
         view.backgroundColor = .white
         
         // Do any additional setup after loading the view, typically from a nib.
-        self.createAlertController()
         uiProfilePicture.layer.cornerRadius = uiChangePhotoButton.layer.cornerRadius
         
         print(uiEditButton.frame)
@@ -33,17 +31,19 @@ class ProfileViewController: UIViewController, UINavigationControllerDelegate, U
     func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [String : Any]) {
         var profileImage: UIImage!
         
-        if  let image = info[UIImagePickerControllerEditedImage] as? UIImage {
+        if    let image = info[UIImagePickerControllerEditedImage] as? UIImage {
             profileImage = image
         }
         else if let image = info[UIImagePickerControllerOriginalImage] as? UIImage {
             profileImage = image
         }
+        
         uiProfilePicture.image = profileImage
+        
         picker.dismiss(animated:true, completion: nil)
     }
     
-    func createImagePickerController(sourceType: UIImagePickerControllerSourceType, allowsEditing: Bool){
+    func createImagePickerController(sourceType: UIImagePickerControllerSourceType, allowsEditing: Bool) {
         if UIImagePickerController.isSourceTypeAvailable(sourceType) {
             uiImagePicker = UIImagePickerController()
             uiImagePicker.delegate = self
@@ -57,24 +57,40 @@ class ProfileViewController: UIViewController, UINavigationControllerDelegate, U
         self.createImagePickerController(sourceType: .camera, allowsEditing: false)
         
         if !(UIImagePickerController.isSourceTypeAvailable(.camera)) {
-            let warningAlert = UIAlertController(
-                title: "Ошибка",
-                message: "Невозможно использовать камеру",
-                preferredStyle: .alert
-            );
-            let closeAction = UIAlertAction(title: "Ок", style: .cancel, handler: nil)
+            let warningAlert = createErrorAlert(message: "Невозможно использовать камеру")
             
-            warningAlert.addAction(closeAction)
             present(warningAlert, animated: true, completion: nil)
         }
     }
     
     func onSelectPhotoFromGallery(action: UIAlertAction) {
         self.createImagePickerController(sourceType: .photoLibrary, allowsEditing: true)
+        
+        if !(UIImagePickerController.isSourceTypeAvailable(.photoLibrary)) {
+            let warningAlert = createErrorAlert(
+                title: "Непредвиденная ошибка",
+                message: "Невозможно получить доступ к галерее"
+            )
+            
+            present(warningAlert, animated: true, completion: nil)
+        }
     }
     
-    func createAlertController() {
-        uiAlert = UIAlertController()
+    func createErrorAlert(title: String = "Ошибка", message: String) -> UIAlertController {
+        let warningAlert = UIAlertController(
+            title: title,
+            message: message,
+            preferredStyle: .alert
+        );
+        let closeAction = UIAlertAction(title: "ОК", style: .cancel, handler: nil)
+        
+        warningAlert.addAction(closeAction)
+        
+        return warningAlert;
+    }
+    
+    func createAlertController() -> UIAlertController {
+        let uiAlert = UIAlertController()
         let capturePhotoAction = UIAlertAction(
             title: "Сделать фото",
             style: .default,
@@ -90,49 +106,25 @@ class ProfileViewController: UIViewController, UINavigationControllerDelegate, U
         uiAlert.addAction(capturePhotoAction)
         uiAlert.addAction(selectPhotoFromGalleryAction)
         uiAlert.addAction(cancelAction)
+        
+        return uiAlert;
     }
     
     @IBAction func handleTapPhotoBtn(_ sender: RoundButton) {
         print("Выбери изображение профиля")
-        
-        present(uiAlert, animated: true, completion: nil)
+        present(createAlertController(), animated: true, completion: nil)
     }
-    
-    func viewWillAppear() {
-        print("Calling \(#function)")
-    }
+  
     
     func viewDidAppear() {
         print("Calling \(#function)")
         
         print(uiEditButton.frame)
     }
-    
-    override func viewWillLayoutSubviews() {
-        super.viewWillLayoutSubviews()
-        
-        print("Calling \(#function)")
-    }
-
-    override func viewDidLayoutSubviews(){
-        super.viewDidLayoutSubviews()
-        
-        print("Calling \(#function)")
-    }
-    
-    func viewWillDisappear() {
-        print("Calling \(#function)")
-    }
-    
-    func viewDidDisappear() {
-        print("Calling \(#function)")
-    }
-    
+   
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
 }
 
